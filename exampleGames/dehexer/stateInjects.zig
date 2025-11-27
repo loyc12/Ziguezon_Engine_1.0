87e76267232abc9f97e04b2d35f26ef22795585b
loyc12 <lordloyc@gmail.com>
2025-11-26 20:56:05 -0500

    added bigger mines. game is now somewhat balanced and fun

--- exampleGames/dehexer/stateInjects.zig
+++ exampleGames/dehexer/stateInjects.zig
@@ -2,12 +2,18 @@ const std = @import( "std" );
 const def = @import( "defs" );
 
 pub var GRID_ID    : u32 = 0;
-pub var MINE_COUNT : u32 = 400;
+pub var MINE_COUNT : u32 = 256;
 
-var GRID_SCALE  : f32 = 32;
+pub var GRID_SCALE  : f32 = 43;
 
-var GRID_WIDTH  : i32 = 64;
-var GRID_HEIGHT : i32 = 32;
+pub var GRID_WIDTH  : i32 = 50;
+pub var GRID_HEIGHT : i32 = 25;
+
+pub const TILE_MINE_1 = def.e_tile_type.T1;
+pub const TILE_MINE_2 = def.e_tile_type.T2;
+pub const TILE_MINE_3 = def.e_tile_type.T3;
+pub const TILE_HIDDEN = def.e_tile_type.T4;
+pub const TILE_SHOWN  = def.e_tile_type.T5;
 
 // ================================ STATE INJECTION FUNCTIONS ================================
 
@@ -24,7 +30,7 @@ pub fn OnOpen( ng : *def.Engine ) void
     .gridSize  = .{ .x = GRID_WIDTH, .y = GRID_HEIGHT },
     .tileScale = .{ .x = GRID_SCALE, .y = GRID_SCALE },
     .tileShape = .HEX1,
-  }, .T1 );
+  }, TILE_HIDDEN);
 
 
   if( tlm == null ){ def.qlog( .ERROR, 0, @src(), "Failed to create tilemap" ); }
@@ -50,9 +56,12 @@ pub fn OnOpen( ng : *def.Engine ) void
     // if value > odds, tile is set as mine ( wall )
     if( remaingingMineCount > 0 and noiseVal < threshold )
     {
-      //tile.colour = .blue;
-      tile.tType  = .T3;
       remaingingMineCount -= 1;
+      const mineTypeNoiseVal = def.G_RNG.getFloat( f32 );
+
+      if(      mineTypeNoiseVal < 0.5 ){ tile.tType  = TILE_MINE_1; tile.colour = .lGreen; }
+      else if( mineTypeNoiseVal < 0.8 ){ tile.tType  = TILE_MINE_2; tile.colour = .mGreen; }
+      else{                              tile.tType  = TILE_MINE_3; tile.colour = .dGreen; }
     }
     remaingingTileCount -= 1;
   }
@@ -65,10 +74,6 @@ pub fn OnOpen( ng : *def.Engine ) void
   {
     def.qlog( .INFO, 0, @src(), "$ Assigned all mines properly !" );
   }
-
-
-
-
 
 }
 
--- exampleGames/dehexer/stepInjects.zig
+++ exampleGames/dehexer/stepInjects.zig
@@ -12,12 +12,18 @@ const Box2   = def.Box2;
 
 // ================================ GLOBAL GAME VARIABLES ================================
 
-const TILE_HIDDEN = def.e_tile_type.T1;
-const TILE_SHOWN  = def.e_tile_type.T2;
-const TILE_MINE   = def.e_tile_type.T3;
+const TILE_MINE_1 = stateInj.TILE_MINE_1;
+const TILE_MINE_2 = stateInj.TILE_MINE_2;
+const TILE_MINE_3 = stateInj.TILE_MINE_3;
+const TILE_HIDDEN = stateInj.TILE_HIDDEN;
+const TILE_SHOWN  = stateInj.TILE_SHOWN;
+
+const NUM_SIZE : f32 = 24;
 
 var FLAG_COUNT : u32 = 0;
-var EXPLODED   : bool = false;
+var LIFE_COUNT : i32 = 5;
+
+var HAS_WON : bool = false;
 
 
 // ================================ HELPER FUNCTIONS ================================
@@ -32,36 +38,174 @@ fn getNeighbourMineCount( ng : *def.Engine, grid : *def.Tilemap, tile : *def.Til
   {
     const nCoords = grid.getNeighbourCoords( tile.gridCoords, dir ) orelse
     {
-      def.log( .TRACE, 0, @src(), "No northern neighbour in direcetion {s} found for tile at {d}:{d} in tilemap {d}",
-              .{ @tagName( dir ), tile.gridCoords.x, tile.gridCoords.y, grid.id });
+      def.log( .TRACE, 0, @src(), "No northern neighbour in direction {s} found for tile at {d}:{d}", .{ @tagName( dir ), tile.gridCoords.x, tile.gridCoords.y });
       continue;
     };
 
     const n = grid.getTile( nCoords ) orelse
     {
-      def.log( .WARN, 0, @src(), "No tile found at {d}:{d} in tilemap {d}", .{ nCoords.x, nCoords.y, grid.id });
+      def.log( .WARN, 0, @src(), "No tile found at {d}:{d}", .{ nCoords.x, nCoords.y });
       continue;
     };
 
-    if( n.tType == TILE_MINE ){ res += 1; }
+    if( n.tType == TILE_MINE_1 ){ res += 1; }
+    if( n.tType == TILE_MINE_2 ){ res += 2; }
+    if( n.tType == TILE_MINE_3 ){ res += 3; }
+  }
+  return res;
+}
+
+fn getNeighbourFlagCount( ng : *def.Engine, grid : *def.Tilemap, tile : *def.Tile ) u32
+{
+  _ = ng;
+
+  var res : u32 = 0;
+
+  for( def.e_dir_2.arr )| dir |
+  {
+    const nCoords = grid.getNeighbourCoords( tile.gridCoords, dir ) orelse
+    {
+      def.log( .TRACE, 0, @src(), "No northern neighbour in direction {s} found for tile at {d}:{d}", .{ @tagName( dir ), tile.gridCoords.x, tile.gridCoords.y });
+      continue;
+    };
+
+    const n = grid.getTile( nCoords ) orelse
+    {
+      def.log( .WARN, 0, @src(), "No tile found at {d}:{d}", .{ nCoords.x, nCoords.y });
+      continue;
+    };
+
+    if( n.colour.isEq( .lBlue ) ){ res += 1; }
+    if( n.colour.isEq( .mBlue ) ){ res += 2; }
+    if( n.colour.isEq( .dBlue ) ){ res += 3; }
+  }
+  return res;
+}
+
+// revealing a tile
+fn leftCLickTile( ng : *def.Engine, grid : *def.Tilemap, tile : *def.Tile ) void
+{
+  // Does nothing if a flagged tile was clicked
+  if( tile.colour.isEq( .lBlue )){  return; }
+  if( tile.colour.isEq( .mBlue  )){ return; }
+  if( tile.colour.isEq( .dBlue )){  return; }
+
+  // Does nothing if an uncovered mine was clicked
+  if( tile.colour.isEq( .yellow )){ return; }
+  if( tile.colour.isEq( .orange )){ return; }
+  if( tile.colour.isEq( .red    )){ return; }
+
+  // Check if a mine was clicked
+  if( tile.tType == TILE_MINE_1 )
+  {
+    tile.colour = .yellow;
+    FLAG_COUNT += 1;
+    LIFE_COUNT -= 1;
+
+    def.log( .INFO, 0, @src(), "@ Clicked on a small mine at {d}:{d}", .{ tile.gridCoords.x, tile.gridCoords.y });
+    return;
+  }
+  if( tile.tType == TILE_MINE_2 )
+  {
+    tile.colour = .orange;
+    FLAG_COUNT += 1;
+    LIFE_COUNT -= 2;
+
+    def.log( .INFO, 0, @src(), "@ Clicked on a medium mine at {d}:{d}", .{ tile.gridCoords.x, tile.gridCoords.y });
+    return;
+  }
+  if( tile.tType == TILE_MINE_3 )
+  {
+    tile.colour = .orange;
+    FLAG_COUNT += 1;
+    LIFE_COUNT -= 3;
+
+    def.log( .INFO, 0, @src(), "@ Clicked on a large mine at {d}:{d}", .{ tile.gridCoords.x, tile.gridCoords.y });
+    return;
   }
 
-  return res;
+  if( tile.tType == TILE_SHOWN )
+  {
+    const tileMineCount = getNeighbourMineCount( ng, grid, tile );
+    const tileFlagCount = getNeighbourFlagCount( ng, grid, tile );
+
+    if( tileFlagCount > 0 and tileFlagCount == tileMineCount )
+    {
+      for( def.e_dir_2.arr )| dir |
+      {
+        const nCoords = grid.getNeighbourCoords( tile.gridCoords, dir ) orelse
+        {
+          def.log( .TRACE, 0, @src(), "No northern neighbour in direction {s} found for tile at {d}:{d} ", .{ @tagName( dir ), tile.gridCoords.x, tile.gridCoords.y });
+          continue;
+        };
+
+        const n = grid.getTile( nCoords ) orelse
+        {
+          def.log( .WARN, 0, @src(), "No tile found at {d}:{d}", .{ nCoords.x, nCoords.y });
+          continue;
+        };
+
+        if( n.tType != TILE_SHOWN ){ leftCLickTile( ng, grid, n ); }
+      }
+    }
+  }
+
+  floodDiscoverCheck( ng, grid, tile );
+
+  if( !HAS_WON and LIFE_COUNT > 0 )
+  {
+    HAS_WON = playerHasWon( ng, grid );
+  }
+}
+
+// (un)flagging a tile
+fn rightCLickTile( ng : *def.Engine, grid : *def.Tilemap, tile : *def.Tile ) void
+{
+  _ = ng;
+  _ = grid;
+
+  // Does nothing if a uncovered tile was clicked
+  if( tile.tType == TILE_SHOWN ){ return; }
+
+  // Does nothing if a uncovered mine was clicked
+  if( tile.colour.isEq( .yellow )
+  or  tile.colour.isEq( .orange )
+  or  tile.colour.isEq( .red    )){ return; }
+
+  // Using current colour to check if flagged or not ( sketchy roundabout bullshit )
+  if(      tile.colour.isEq( .lBlue )){ tile.colour = .mBlue; }
+  else if( tile.colour.isEq( .mBlue )){ tile.colour = .dBlue; }
+  else if( tile.colour.isEq( .dBlue ))
+  {
+    def.log( .INFO, 0, @src(), "# Adding a flag on {d}:{d}", .{ tile.gridCoords.x, tile.gridCoords.y });
+    FLAG_COUNT += 1;
+    tile.colour = .lGray;
+  }
+  else
+  {
+    def.log( .INFO, 0, @src(), "# Removing a flag on {d}:{d}", .{ tile.gridCoords.x, tile.gridCoords.y });
+    tile.colour = .lBlue;
+    FLAG_COUNT += 1;
+  }
 }
 
 
 fn floodDiscoverCheck( ng : *def.Engine, grid : *def.Tilemap, tile : *def.Tile ) void
 {
   // Recursion safety bound
-  if( tile.tType == TILE_MINE or tile.tType == TILE_SHOWN ){ return; }
+  if( tile.tType != TILE_HIDDEN  ){ return; }
 
   // Setting the tile as discovered
-  tile.tType  = TILE_SHOWN;
+  tile.tType = TILE_SHOWN;
 
+  if( tile.colour.isEq( .lBlue )
+  or  tile.colour.isEq( .mBlue )
+  or  tile.colour.isEq( .dBlue )){ FLAG_COUNT -= 1; }
+
+  tile.colour = .pGray;
   // finding the tile's new colour
   const nMineCount = getNeighbourMineCount( ng, grid, tile );
 
-  tile.colour = .pGray;
 
   // Don't recursively call checks neighbour is there is any mines around
   if( nMineCount > 0 ){ return; }
@@ -71,7 +215,7 @@ fn floodDiscoverCheck( ng : *def.Engine, grid : *def.Tilemap, tile : *def.Tile )
   {
     const nCoords = grid.getNeighbourCoords( tile.gridCoords, dir ) orelse
     {
-      def.log( .TRACE, 0, @src(), "No northern neighbour in direcetion {s} found for tile at {d}:{d}",
+      def.log( .TRACE, 0, @src(), "No northern neighbour in direction {s} found for tile at {d}:{d}",
               .{ @tagName( dir ), tile.gridCoords.x, tile.gridCoords.y});
       continue;
     };
@@ -83,10 +227,25 @@ fn floodDiscoverCheck( ng : *def.Engine, grid : *def.Tilemap, tile : *def.Tile )
     };
 
     // Don't call self on mines
-    if( n.tType == TILE_MINE ){ continue; }
+    if( n.tType == TILE_MINE_1
+    or  n.tType == TILE_MINE_2
+    or  n.tType == TILE_MINE_3 ){ continue; }
 
     floodDiscoverCheck( ng, grid, n );
   }
+}
+
+fn playerHasWon( ng : *def.Engine, grid : *def.Tilemap ) bool
+{
+  _ = ng;
+
+  for( 0 .. grid.getTileCount() )| index |
+  {
+    const tile : *def.Tile = &grid.tileArray.items.ptr[ index ];
+
+    if( tile.tType == TILE_HIDDEN ){ return false; }
+  }
+  return true;
 }
 
 
@@ -107,77 +266,31 @@ pub fn OnUpdateInputs( ng : *def.Engine ) void
     return;
   };
 
-  // If left clicked, try to uncover the tile
-  if( def.ray.isMouseButtonPressed( def.ray.MouseButton.left ))
+  if( HAS_WON or LIFE_COUNT <= 0 )
   {
-    const mouseScreenPos = def.ray.getMousePosition();
-    const mouseWorldPos  = def.ray.getScreenToWorld2D( mouseScreenPos, ng.getCameraCpy().?.toRayCam() );
-
-    const worldCoords = grid.findHitTileCoords( Vec2{ .x = mouseWorldPos.x, .y = mouseWorldPos.y });
-
-    if( worldCoords != null )
-    {
-      def.log( .INFO, 0, @src(), "Clicked on tile at {d}:{d}", .{ worldCoords.?.x, worldCoords.?.y });
-
-      var clickedTile = grid.getTile( worldCoords.? ) orelse
-      {
-        def.log( .WARN, 0, @src(), "No tile found at {d}:{d}", .{ worldCoords.?.x, worldCoords.?.y });
-        return;
-      };
-
-      // Does nothing if a flagged tile was clicked
-      if( clickedTile.colour.isEq( .blue )){ return; }
-
-      // Check if a mine was clicked
-      if( clickedTile.tType == TILE_MINE )
-      {
-        clickedTile.colour = .red;
-        EXPLODED = true;
-
-        def.log( .INFO, 0, @src(), "@ Clicked on a mine at {d}:{d}", .{ worldCoords.?.x, worldCoords.?.y });
-        return;
-      }
-
-      floodDiscoverCheck( ng, grid, clickedTile );
-    }
+    // prevent further action if game was won or lost
+    return;
   }
 
-  // If right clicked, mark the tile as flagged
-  if( def.ray.isMouseButtonPressed( def.ray.MouseButton.right ))
+  if( def.ray.isMouseButtonPressed( def.ray.MouseButton.left ) or def.ray.isMouseButtonPressed( def.ray.MouseButton.right ))
   {
     const mouseScreenPos = def.ray.getMousePosition();
     const mouseWorldPos  = def.ray.getScreenToWorld2D( mouseScreenPos, ng.getCameraCpy().?.toRayCam() );
 
     const worldCoords = grid.findHitTileCoords( Vec2{ .x = mouseWorldPos.x, .y = mouseWorldPos.y });
 
-    if( worldCoords != null )
+    if( worldCoords == null ){ return; }
+
+    def.log( .INFO, 0, @src(), "Clicked on tile at {d}:{d}", .{ worldCoords.?.x, worldCoords.?.y });
+
+    const clickedTile = grid.getTile( worldCoords.? ) orelse
     {
-      def.log( .INFO, 0, @src(), "Clicked on tile at {d}:{d}", .{ worldCoords.?.x, worldCoords.?.y });
+      def.log( .WARN, 0, @src(), "No tile found at {d}:{d}", .{ worldCoords.?.x, worldCoords.?.y });
+      return;
+    };
 
-      var clickedTile = grid.getTile( worldCoords.? ) orelse
-      {
-        def.log( .WARN, 0, @src(), "No tile found at {d}:{d}", .{ worldCoords.?.x, worldCoords.?.y });
-        return;
-      };
-
-      // Does nothing if a uncovered tile was clicked
-      if( clickedTile.tType == TILE_SHOWN ){ return; }
-
-      // Does nothing if a uncovered mine was clicked
-      if( clickedTile.colour.isEq( .red )){ return; }
-
-      // Using current colour to check if flagged or not ( sketchy roundabout bullshit )
-      if( clickedTile.colour.isEq( .blue ))
-      {
-        clickedTile.colour = .lGray;
-        FLAG_COUNT -= 1;
-      }
-      else
-      {
-        clickedTile.colour = .blue;
-        FLAG_COUNT += 1;
-      }
-    }
+    if( def.ray.isMouseButtonPressed( def.ray.MouseButton.left  )){ leftCLickTile(  ng, grid, clickedTile ); }
+    if( def.ray.isMouseButtonPressed( def.ray.MouseButton.right )){ rightCLickTile( ng, grid, clickedTile ); }
   }
 }
 
@@ -211,6 +324,8 @@ pub fn OnRenderOverlay( ng : *def.Engine ) void
 {
   var mineBuff : [ 16:0 ]u8 = .{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
   var flagBuff : [ 16:0 ]u8 = .{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  var lifeBuff : [ 16:0 ]u8 = .{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
 
   const mineCountSlice = std.fmt.bufPrint( &mineBuff, "Mines : {d}", .{ stateInj.MINE_COUNT }) catch | err |
   {
@@ -222,21 +337,26 @@ pub fn OnRenderOverlay( ng : *def.Engine ) void
       def.log(.ERROR, 0, @src(), "Failed to format flagCount : {}", .{err});
       return;
   };
+  const lifeCountSlice = std.fmt.bufPrint( &lifeBuff, "Lives : {d}", .{ LIFE_COUNT }) catch | err |
+  {
+      def.log(.ERROR, 0, @src(), "Failed to format lifeCount : {}", .{err});
+      return;
+  };
 
   mineBuff[ mineCountSlice.len ] = 0;
   flagBuff[ flagCountSlice.len ] = 0;
-
+  lifeBuff[ lifeCountSlice.len ] = 0;
 
   const cam = ng.getCameraCpy() orelse
   {
     def.qlog( .ERROR, 0, @src(), "No main camera initialized" );
     return;
   };
-  const screenCenter = def.ray.getWorldToScreen2D( .{ .x = 0, .y = 0 }, cam.toRayCam() ).x;
+  const screenCenter = def.ray.getWorldToScreen2D( .{ .x = 0, .y = 0 }, cam.toRayCam() );
 
-  def.drawCenteredText( &mineBuff, screenCenter - 256, 64, 32, def.Colour.red );
-  def.drawCenteredText( &flagBuff, screenCenter + 256, 64, 32, def.Colour.red );
-
+  def.drawCenteredText( &mineBuff, screenCenter.x - 320, 64, 32, def.Colour.red   );
+  def.drawCenteredText( &flagBuff, screenCenter.x,       64, 32, def.Colour.blue  );
+  def.drawCenteredText( &lifeBuff, screenCenter.x + 320, 64, 32, def.Colour.green );
 
 
   var grid = ng.getTilemap( stateInj.GRID_ID ) orelse
@@ -248,12 +368,19 @@ pub fn OnRenderOverlay( ng : *def.Engine ) void
   for( 0 .. grid.getTileCount() )| index |
   {
     const tile : *def.Tile = &grid.tileArray.items.ptr[ index ];
+
+    const tileCenter = def.ray.getWorldToScreen2D( grid.getAbsTilePos( tile.gridCoords ).toRayVec2(), cam.toRayCam() );
+
+    if(      tile.colour.isEq( .lBlue )){ def.drawCenteredText( "1", tileCenter.x, tileCenter.y, NUM_SIZE, .white ); }
+    else if( tile.colour.isEq( .mBlue )){ def.drawCenteredText( "2", tileCenter.x, tileCenter.y, NUM_SIZE, .white ); }
+    else if( tile.colour.isEq( .dBlue )){ def.drawCenteredText( "3", tileCenter.x, tileCenter.y, NUM_SIZE, .white ); }
+
     if( tile.tType != TILE_SHOWN ){ continue; }
 
     const nMineCount = getNeighbourMineCount( ng, grid, tile );
     if( nMineCount == 0 ){ continue; }
 
-    var numBuff : [ 1:0 ]u8 = .{ 0 };
+    var numBuff : [ 2:0 ]u8 = .{ 0, 0 };
 
     const numSlice = std.fmt.bufPrint( &numBuff, "{d}", .{ nMineCount }) catch | err |
     {
@@ -266,16 +393,29 @@ pub fn OnRenderOverlay( ng : *def.Engine ) void
     const numCol = switch( nMineCount )
     {
       1    => def.Colour.cerul,
-      2    => def.Colour.green,
-      3    => def.Colour.yellow,
-      4    => def.Colour.orange,
-      5    => def.Colour.red,
-      6    => def.Colour.magenta,
-      else => def.Colour.red,
+      2    => def.Colour.cyan,
+      3    => def.Colour.green,
+      4    => def.Colour.yellow,
+      5    => def.Colour.gold,
+      6    => def.Colour.orange,
+      7    => def.Colour.red,
+      8    => def.Colour.fuchsia,
+      9    => def.Colour.purple,
+      else => def.Colour.indigo,
     };
 
-    const tileCenter = def.ray.getWorldToScreen2D( grid.getAbsTilePos( tile.gridCoords ).toRayVec2(), cam.toRayCam() );
+    def.drawCenteredText( &numBuff, tileCenter.x, tileCenter.y, NUM_SIZE, numCol );
 
-    def.drawCenteredText( &numBuff, tileCenter.x, tileCenter.y, 20, numCol );
+  }
+
+  if( LIFE_COUNT <= 0 )
+  {
+    def.coverScreenWithCol( def.Colour.new( 0, 0, 0, 128 ));
+    def.drawCenteredText( "L + SKILL ISSUE + WOMP WOMP + LMFAO + STAY MAD", screenCenter.x, screenCenter.y, 50, .red );
+  }
+  else if( HAS_WON )
+  {
+    def.coverScreenWithCol( def.Colour.new( 0, 0, 0, 128 ));
+    def.drawCenteredText( "W + SKILLFUL + HELL YEAH + ROFL + STAY GLAD", screenCenter.x, screenCenter.y, 50, .green );
   }
 }
