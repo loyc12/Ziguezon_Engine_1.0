5455d06db9cae7f6023e30571661d4fe8181d37c
loyc12 <lordloyc@gmail.com>
2026-01-14 20:42:01 -0500

    WIP on the file reader / stdin reader

---
+++ src/utils/inputer.zig
@@ -0,0 +1,46 @@
+const std = @import( "std" );
+const def = @import( "defs" );
+
+// TODO : figure me out better
+
+pub fn demoStdin( allocator : std.mem.Allocator, useFixed : bool ) !void
+{
+  // buffered or unbuffered, buffer when doing many small reads
+  const buffered : bool     = true;
+  var   buffer   : [ 4096 ]u8 = undefined;
+
+  const read_buffer = if( buffered ) &buffer else &.{};
+
+  var input_reader : std.fs.File.Reader = std.fs.File.stdin().reader( read_buffer );
+
+  // IMPORTANT: capture an interface pointer
+  const reader: *std.Io.Reader = &input_reader.interface;
+
+  const limit = 1024;
+  if( useFixed )
+  {
+    // must be large enough for read to succeed
+    var write_buffer : [ 1024 ]u8 = undefined;
+    var writer_fixed = std.Io.Writer.fixed( &write_buffer );
+
+    const len = try reader.streamDelimiterLimit( &writer_fixed, '\n', .limited( limit ));
+    std.debug.print( "Read fixed: {d}:{s}\n", .{ len, writer_fixed.buffered() });
+  }
+  else
+  {
+    var writer_alloc = std.Io.Writer.Allocating.init( allocator );
+    defer writer_alloc.deinit();
+
+    const writer = &writer_alloc.writer;
+
+    const len = try reader.streamDelimiterLimit( writer, '\n', .limited( limit ));
+    std.debug.print("Read alloc: {d}:{s}\n", .{ len, writer_alloc.written() });
+  }
+}
+
+const fileReadData = struct
+{
+  isInit      : bool       = false,
+  readBuffer  : [ 4096 ]u8 = undefined,
+  writeBuffer : [ 4096 ]u8 = undefined,
+};
--- src/utils/outputer.zig
+++ src/utils/outputer.zig
@@ -5,12 +5,21 @@ const def = @import( "defs" );
 
 var outputBuffer : [ 4096 ]u8 = undefined;
 var errorBuffer  : [ 4096 ]u8 = undefined;
-var inputBuffer  : [ 4096 ]u8 = undefined;
 
 // Buffered for multiple small writes
 pub fn outBuf() !void
 {
   var output_writer: std.fs.File.Writer = std.fs.File.stdout().writer( outputBuffer );
+
+  // IMPORTANT: capture an interface pointer
+  const writer: *std.Io.Writer = &output_writer.interface;
+
+  try writer.writeAll( "Hello world\n" );
+  try writer.flush();
+}
+pub fn errBuf() !void
+{
+  var output_writer: std.fs.File.Writer = std.fs.File.stderr().writer( errorBuffer );
 
   // IMPORTANT: capture an interface pointer
   const writer: *std.Io.Writer = &output_writer.interface;
@@ -30,33 +39,13 @@ pub fn outUnbuf() !void
   try writer.writeAll( "Hello world\n" );
   try writer.flush();
 }
+pub fn errUnbuf() !void
+{
+  var output_writer: std.fs.File.Writer = std.fs.File.stderr().writer( &.{} );
 
-//pub fn demoStdin(allocator: std.mem.Allocator, useFixed: bool) !void
-//{
-//  // buffered or unbuffered, buffer when doing many small reads
-//  const buffered = true;
-//  var buffer: [4096]u8 = undefined;
-//  const read_buffer = if (buffered) &buffer else &.{};
-//
-//  var input_reader: std.fs.File.Reader = std.fs.File.stdin().reader(read_buffer);
-//  // IMPORTANT: capture an interface pointer
-//  const reader: *std.Io.Reader = &input_reader.interface;
-//
-//  const limit = 1024;
-//  if (useFixed)
-//  {
-//    // must be large enough for read to succeed
-//    var write_buffer: [1024]u8 = undefined;
-//    var writer_fixed = std.Io.Writer.fixed(&write_buffer);
-//    const len = try reader.streamDelimiterLimit(&writer_fixed, '\n', .limited(limit));
-//    std.debug.print("Read fixed: {d}:{s}\n", .{ len, writer_fixed.buffered() });
-//  }
-//  else
-//  {
-//    var writer_alloc = std.Io.Writer.Allocating.init(allocator);
-//    defer writer_alloc.deinit();
-//    const writer = &writer_alloc.writer;
-//    const len = try reader.streamDelimiterLimit(writer, '\n', .limited(limit));
-//    std.debug.print("Read alloc: {d}:{s}\n", .{ len, writer_alloc.written() });
-//  }
-//}
+  // IMPORTANT: capture an interface pointer
+  const writer: *std.Io.Writer = &output_writer.interface;
+
+  try writer.writeAll( "Hello world\n" );
+  try writer.flush();
+}
