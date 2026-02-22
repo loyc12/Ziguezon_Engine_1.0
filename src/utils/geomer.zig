ca34f832ecf27d94839a01c35aedd582151393bd
loyc12 <lordloyc@gmail.com>
2026-02-22 17:57:04 -0500

    still WIP for geom3D

--- src/utils/geomer.zig
+++ src/utils/geomer.zig
@@ -16,6 +16,9 @@ const PI = def.PI;
 
 pub const Geom2D = enum( u8 )
 {
+  // NOTE : for polygons, "s" param scales the circumradius in X and Y
+  ///       Aka : Affine-stretched regular polygon ( ellipse-affine model )
+
   RECT, // Square / Rectangle
 
   TRIA, // Triangle
@@ -53,21 +56,83 @@ pub const Geom2D = enum( u8 )
   // Sum of all edges
   pub inline fn getPerim( self : Geom2D, s : Vec2 ) f32
   {
-    return switch( self )
+    switch( self )
     {
-      .RECT => ( s.X + s.Y ) * 2,
-      else  => ,// Use polygon formula ( no exact one for ellipses
-    };
+      .RECT => return ( s.X + s.Y ) * 2,
+      .ELLI =>
+      {
+        if( @abs( s.X - s.Y ) < def.EPS ) // Circle circumference from radius
+        {
+          return 2.0 * PI * s.X;
+        }
+        else // Ramanujan's 2nd ellipse perimeter approximation ( Relative error <≈ 0.0003% )
+        {
+          const sum = s.X + s.Y;
+          const dif = s.X - s.Y;
+
+          // Ellipticity parameter
+          const h = ( dif * dif ) / ( sum * sum );
+
+          // Ramanujan correction factor
+          const C = 1.0 + ( 3.0 * h ) / ( 10.0 + @sqrt( 4.0 - 3.0 * h ));
+
+          return C * PI * sum;
+        }
+      },
+
+      else =>
+      {
+        const N : f32 = @floatFromInt( self.getEdgeCount() );
+
+        if( @abs( s.X - s.Y ) < def.EPS ) // Regular polygon perimeter from circumradius ( trigonometric form )
+        {
+          return ( 2.0 * N ) * s.X * @sin( PI / N );
+        }
+        else // Chebyshev ellipse correction heuristic for affine regular polygon perimeter ( Relative error <≈ 1% for exentricity of 10 )
+        {
+          const sum = s.X + s.Y;
+          const dif = s.X - s.Y;
+
+          // Geometric mean radius
+          const mR = @sqrt( s.X * s.Y );
+
+          // Ellipticity parameter
+          const h1 = ( dif * dif ) / ( sum * sum );
+
+          // Chebyshev correction factor
+          const h2 = h1 * h1;
+          const h3 = h1 * h2;
+          const h4 = h1 * h3;
+
+          const C = 1.0 + ( 0.25 * h1 ) - ( 0.0625 * h2 ) - ( 0.015625 * h3 ) + ( 0.00390625 * h4 );
+
+          return C * ( 2.0 * N ) * mR * @sin( PI / N );
+        }
+      },
+    }
   }
 
   pub inline fn getArea( self : Geom2D, s : Vec2 ) f32
   {
-    return switch( self )
+    switch( self )
     {
-      .RECT => s.X * s.Y,
-      .ELLI => PI * s.X * s.Y,
-      else  => ,// Use polygon formula
-    };
+      .RECT => return s.X * s.Y,
+      .ELLI => return PI * s.X * s.Y,
+      else  =>
+      {
+        const N : f32 = @floatFromInt( self.getEdgeCount() );
+
+        if( @abs( s.X - s.Y ) < def.EPS ) // Regular polygon area from circumradius ( trigonometric form )
+        {
+          return N * s.X * s.X * @sin( PI / N ) * @cos( PI / N );
+        }
+        else // Affine-stretched polygon area ( exact )
+        {
+          const sin2piN = @sin( 2 * PI / N );
+          return 0.5 * N * s.X * s.Y * sin2piN;
+        }
+      },
+    }
   }
 };
 
@@ -93,7 +158,7 @@ const Geom3D = enum( u8 )
 
   // F + V = E + 2
 
-  pub inline fn getVertCount( self : Geom2D, baseShape : ?Geom2D ) u16
+  pub inline fn getVertCount( self : Geom3D, baseShape : ?Geom2D ) u16
   {
     switch( self )
     {
@@ -120,7 +185,7 @@ const Geom3D = enum( u8 )
     return 0;
   }
 
-  pub inline fn getEdgeCount( self : Geom2D, baseShape : ?Geom2D ) u16
+  pub inline fn getEdgeCount( self : Geom3D, baseShape : ?Geom2D ) u16
   {
     switch( self )
     {
@@ -147,7 +212,7 @@ const Geom3D = enum( u8 )
     return 0;
   }
 
-  pub inline fn getFaceCount( self : Geom2D, baseShape : ?Geom2D ) u16
+  pub inline fn getFaceCount( self : Geom3D, baseShape : ?Geom2D ) u16
   {
     switch( self )
     {
@@ -175,18 +240,86 @@ const Geom3D = enum( u8 )
   }
 
   // Sum of all edges
-  pub inline fn getPerim( self : Geom2D, s : Vec3, baseShape : ?Geom2D ) f32
+  pub inline fn getPerim( self : Geom3D, s : Vec3, baseShape : ?Geom2D ) f32
   {
+    switch( self )
+    {
+      .TETRA => return ,
+      .CUBE  => return 4.0 * ( s.X + s.Y + s.Z ),
+      .OCTA  => return ,
+      .DODE  => return ,
+      .ICOSA => return ,
 
+      .SPHER => return 0,
+      .TORUS => return 0,
+      .CONE  => return Geom2D.ELLI.getPerim( .new( s.X, s.Z )),
+      .CYLIN => return Geom2D.ELLI.getPerim( .new( s.X, s.Z )) * 2.0,
+
+      else => if( baseShape != null )
+      {
+        if( self == .PYRAM )
+        {
+          const bP = baseShape.?.getPerim( .new( s.X, s.Z )); // TODO : Compensate for slanting
+          const bV = baseShape.?.getEdgeCount() * s.Y;
+
+          return (( 2.0 * bP ) + ( s.Y * bV )) * 0.5;
+        }
+        if( self == .PRISM )
+        {
+          const bP = baseShape.?.getPerim( .new( s.X, s.Z ));
+          const bV = baseShape.?.getEdgeCount() * s.Y;
+
+          return ( 2.0 * bP ) + ( s.Y * bV );
+        }
+      }
+    }
+
+    // TODO : log error
+
+    return 0;
   }
 
   // Sum of all faces
-  pub inline fn getArea( self : Geom2D, s : Vec3, baseShape : ?Geom2D ) f32
+  pub inline fn getArea( self : Geom3D, s : Vec3, baseShape : ?Geom2D ) f32
   {
+    switch( self )
+    {
+      .TETRA => return ,
+      .CUBE  => return 2.0 * (( s.X * s.Y ) + ( s.X * s.Z ) + ( s.Y * s.Z )),
+      .OCTA  => return ,
+      .DODE  => return ,
+      .ICOSA => return ,
 
+      .SPHER => return 4.0 * PI * s.X * s.Y * s.Z, // TODO : find better aprox / test for spheroid
+      .TORUS => return ,
+      .CONE  => return ,
+      .CYLIN => return ,
+
+      else => if( baseShape != null )
+      {
+        if( self == .PYRAM )
+        {
+          const bP = baseShape.?.getPerim( .new( s.X, s.Z ));
+          const bA = baseShape.?.getArea(  .new( s.X, s.Z ));
+
+          return (( 2.0 * bA ) + ( s.Y * bP )) * 0.5; // TODO : Compensate for slanting
+        }
+        if( self == .PRISM )
+        {
+          const bP = baseShape.?.getPerim( .new( s.X, s.Z ));
+          const bA = baseShape.?.getArea(  .new( s.X, s.Z ));
+
+          return ( 2.0 * bA ) + ( s.Y * bP );
+        }
+      }
+    }
+
+    // TODO : log error
+
+    return 0;
   }
 
-  pub inline fn getVolume( self : Geom2D, s : Vec3, baseShape : ?Geom2D ) f32
+  pub inline fn getVolume( self : Geom3D, s : Vec3, baseShape : ?Geom2D ) f32
   {
     switch( self )
     {
